ecdd9689b90ac2625a0c780d39c851d1c9b2035d
Federico Spinelli <fspinelli@gmail.com>
2015-12-03 12:10:54 +0100

    Updated ycm_extra_conf with new twmplates

--- ycm/dot-ycm_extra_conf.py
+++ ycm/dot-ycm_extra_conf.py
@@ -1,20 +1,47 @@
-import os, inspect
+# This file is NOT licensed under the GPLv3, which is the license for the rest
+# of YouCompleteMe.
+#
+# Here's the license text for this file:
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org/>
+
+import os
 import ycm_core
 
+# These are the compilation flags that will be used in case there's no
+# compilation database set (by default, one is not set).
+# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
 '-Wall',
-# # '-Wextra',
-# # '-Werror',
-# # '-Wc++98-compat',
-# '-Wno-long-long',
-# '-Wno-unused-variable',
-# '-Wno-variadic-macros',
-# '-fexceptions',
-# '-DNDEBUG',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
+'-Wextra',
+'-Wno-long-long',
+'-Wno-variadic-macros',
+'-Wno-deprecated',
+'-fexceptions',
+'-DNDEBUG',
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
 '-std=c++11',
@@ -24,14 +51,21 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
-'-I',
-'.',
-'-isystem',
-'/usr/include',
-'-isystem',
-'/usr/local/include',
-'-isystem',
+'-I/home/tabjones/catkin/devel/include',
+'-I/home/tabjones/catkin/src/calibration/include',
+'-I/home/tabjones/catkin/src/gaussian-object-modelling/include',
+'-I/home/tabjones/catkin/src/gaussian-object-modelling/src/include',
+'-I/home/tabjones/catkin/src/pel/include',
+'-I/home/tabjones/catkin/src/pacman_vision/node/include',
+'-I/opt/ros/indigo/include',
+'-I/usr/include/eigen3',
+'-I/usr/include/ni',
+'-I/usr/include/openni2',
+'-I/usr/include/vtk-5.8',
+'-I/usr/local/include/pcl-1.7',
+'-I/usr/include/pcl-1.7'
 ]
+
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
@@ -43,14 +77,14 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = '/home/tabjones/catkin/build/'
+compilation_database_folder = ''
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
 else:
   database = None
 
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c']
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
 
 def DirectoryOfThisScript():
   return os.path.dirname( os.path.abspath( __file__ ) )
@@ -97,22 +131,13 @@ def GetCompilationInfoForFile( filename ):
   # should be good enough.
   if IsHeaderFile( filename ):
     basename = os.path.splitext( filename )[ 0 ]
-    ## search also for ROS paradigm  (headers in ./include/pck_name/  and sources in ./src/ )
-    tail = os.path.split (basename) [ 1 ]
-    basedir = os.path.dirname ( os.path.dirname( os.path.dirname( basename ) ) )
     for extension in SOURCE_EXTENSIONS:
       replacement_file = basename + extension
-      filen = tail + extension
-      rep_file = os.path.join( basedir, 'src', filen )
       if os.path.exists( replacement_file ):
         compilation_info = database.GetCompilationInfoForFile(
           replacement_file )
         if compilation_info.compiler_flags_:
           return compilation_info
-      elif os.path.exists( rep_file ):
-        comp_info = database.GetCompilationInfoForFile( rep_file )
-        if comp_info.compiler_flags_:
-          return comp_info
     return None
   return database.GetCompilationInfoForFile( filename )
 
@@ -129,13 +154,6 @@ def FlagsForFile( filename, **kwargs ):
       compilation_info.compiler_flags_,
       compilation_info.compiler_working_dir_ )
 
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-    try:
-      final_flags.remove( '-stdlib=libc++' )
-    except ValueError:
-      pass
   else:
     relative_to = DirectoryOfThisScript()
     final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
