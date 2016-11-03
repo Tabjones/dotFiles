57eeba0f09a8c5f8b580fcd8642713d2f08faedd
Federico Spinelli <fspinelli@qdrobotics.com>
2016-11-03 14:23:56 +0100

    Add install script

---
+++ ubuntu/install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo apt-get install -y aptitude
+sudo aptitude update
+sudo aptitude upgrade -y
+sudo aptitude install -y keychain gitg vim python3-dev python2-dev build-essentials cmake nodejs npm
