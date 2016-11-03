94e3f65ae1dbdd34e6a22349699fcf17ade3fc0d
Federico Spinelli <fspinelli@qdrobotics.com>
2016-11-03 18:07:25 +0100

    Add a gitattributes this repo must maintain LF, further toying with prompt

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -2,4 +2,6 @@
 sudo apt-get install -y aptitude
 sudo aptitude update
 sudo aptitude upgrade -y
-sudo aptitude install -y keychain gitg vim python3-dev python-dev build-essential cmake nodejs npm fortune-mod cowsay curl par
+sudo aptitude install -y keychain gitg vim python3-dev python-dev build-essential
+sudo aptitude install -y cmake nodejs npm fortune-mod cowsay curl par lolcat
+sudo aptitude install -y dos2unix
