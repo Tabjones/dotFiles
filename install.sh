3da2fa50b27c840a3c1ce01ef9d0c3b169f7ed08
Federico Spinelli <fspinelli@gmail.com>
2017-01-12 22:08:00 +0100

    Update vimrc, remove zsh

--- install.sh
+++ install.sh
@@ -18,26 +18,26 @@ fail () {
 
 info 'Backup old dotfiles'
 info '==================='
-mv ${HOME}/.vim ${HOME}/dotFiles/backup/dotVim -f
-mv ${HOME}/.vimrc ${HOME}/dotFiles/backup/dotVimrc -f
-mv ${HOME}/.gvimrc ${HOME}/dotFiles/backup/dotGVimrc -f
-mv ${HOME}/.gitconfig ${HOME}/dotFiles/backup/dotGitconfig -f
-mv ${HOME}/.zshrc ${HOME}/dotFiles/backup/dotZshrc -f
+#mv ${HOME}/.vim ${HOME}/dotFiles/backup/dotVim -f
+#mv ${HOME}/.vimrc ${HOME}/dotFiles/backup/dotVimrc -f
+#mv ${HOME}/.gvimrc ${HOME}/dotFiles/backup/dotGVimrc -f
+#mv ${HOME}/.gitconfig ${HOME}/dotFiles/backup/dotGitconfig -f
+#mv ${HOME}/.zshrc ${HOME}/dotFiles/backup/dotZshrc -f
 
 info 'Setting up Symlinks in HOME'
 info '==========================='
 ln -sfn ${HOME}/dotFiles/vim ${HOME}/.vim
 ln -sf ${HOME}/dotFiles/vim/vimrc ${HOME}/.vimrc
 ln -sf ${HOME}/dotFiles/vim/gvimrc ${HOME}/.gvimrc
-ln -sf ${HOME}/dotFiles/git/gitconfig ${HOME}/.gitconfig
-cp -f ${HOME}/dotFiles/zsh/zshrc ${HOME}/.zshrc
+#ln -sf ${HOME}/dotFiles/git/gitconfig ${HOME}/.gitconfig
+#cp -f ${HOME}/dotFiles/zsh/zshrc ${HOME}/.zshrc
 success ' '
 
 #Vim
 cd ${HOME}/.vim
 ./scripts/setup.sh
 
-cd ${HOME}/dotFiles/zsh
-./setup.sh
+#cd ${HOME}/dotFiles/zsh
+#./setup.sh
 
 cd  ..
